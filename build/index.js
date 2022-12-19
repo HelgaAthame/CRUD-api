48564d4ac4aa7540b13be543fc434306ed5d49d6
HelgaAthame <athamethedarkest@gmail.com>
2022-12-20 00:39:22 +0300

    feat: config data sharing among clusters

--- build/index.js
+++ build/index.js
@@ -22,15 +22,35 @@ var __importStar = (this && this.__importStar) || function (mod) {
     __setModuleDefault(result, mod);
     return result;
 };
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+var _a;
 Object.defineProperty(exports, "__esModule", { value: true });
+const cluster_1 = __importDefault(require("cluster"));
 const http_1 = require("http");
+const os_1 = require("os");
 const url = __importStar(require("url"));
+const fs_1 = require("fs");
 let uuid = require('node-uuid');
 const host = 'localhost';
-const port = process.env.PORT || 4000;
+let port = Number(process.env.PORT) || 4000;
 const endPoint = '/api/users';
-let users = [];
+let users;
+(0, fs_1.readFile)('src/data.json', (err, data) => {
+    users = JSON.parse(data.toString());
+});
 const idRegEx = /[0-9a-f]{8}(-[0-9a-f]{4}){3}-[0-9a-f]{12}$/;
+const args = process.argv;
 const server = (0, http_1.createServer)((req, res) => {
     var _a, _b, _c, _d, _e, _f;
     try {
@@ -41,9 +61,12 @@ const server = (0, http_1.createServer)((req, res) => {
         }
         else if (req.method === 'GET') {
             if (myUrl.path === endPoint) {
-                res.statusCode = 200;
-                res.writeHead(res.statusCode, { 'Content-Type': 'application/json' });
-                res.end(JSON.stringify(users));
+                (0, fs_1.readFile)('src/data.json', (err, data) => {
+                    users = JSON.parse(data.toString());
+                    res.statusCode = 200;
+                    res.writeHead(res.statusCode, { 'Content-Type': 'application/json' });
+                    res.end(JSON.stringify(users));
+                });
             }
             else if ((_b = myUrl.path) === null || _b === void 0 ? void 0 : _b.startsWith(`${endPoint}/`)) {
                 const findUserId = (_c = myUrl.path) === null || _c === void 0 ? void 0 : _c.match(idRegEx);
@@ -53,26 +76,29 @@ const server = (0, http_1.createServer)((req, res) => {
                 }
                 else {
                     const strFindId = JSON.stringify(findUserId[0]);
-                    if (users.length === 0) {
-                        res.statusCode = 404;
-                        res.end('Record with any userId doesn\'t exist');
-                    }
-                    else {
-                        users.forEach((user, i) => {
-                            const strUserId = JSON.stringify(user.id);
-                            if (strFindId === strUserId) {
-                                res.statusCode = 200;
-                                res.writeHead(res.statusCode, { 'Content-Type': 'application/json' });
-                                res.end(JSON.stringify(user));
-                            }
-                            else {
-                                if (i === users.length - 1 && strFindId !== strUserId) {
-                                    res.statusCode = 404;
-                                    res.end('Record with this userId doesn\'t exist');
+                    (0, fs_1.readFile)('src/data.json', (err, data) => {
+                        users = JSON.parse(data.toString());
+                        if (users.length === 0) {
+                            res.statusCode = 404;
+                            res.end('Record with any userId doesn\'t exist');
+                        }
+                        else {
+                            users.forEach((user, i) => {
+                                const strUserId = JSON.stringify(user.id);
+                                if (strFindId === strUserId) {
+                                    res.statusCode = 200;
+                                    res.writeHead(res.statusCode, { 'Content-Type': 'application/json' });
+                                    res.end(JSON.stringify(user));
                                 }
-                            }
-                        });
-                    }
+                                else {
+                                    if (i === users.length - 1 && strFindId !== strUserId) {
+                                        res.statusCode = 404;
+                                        res.end('Record with this userId doesn\'t exist');
+                                    }
+                                }
+                            });
+                        }
+                    });
                 }
             }
             else if (!((_d = myUrl.path) === null || _d === void 0 ? void 0 : _d.startsWith(`${endPoint}/`))) {
@@ -96,9 +122,13 @@ const server = (0, http_1.createServer)((req, res) => {
                         if (myUrl.path === endPoint || myUrl.path === `${endPoint}/`) {
                             newUser.id = uuid.v1();
                             users.push(newUser);
+                            (0, fs_1.writeFile)('src/data.json', JSON.stringify(users), (err) => {
+                                if (err)
+                                    throw new Error('Error while writing users');
+                            });
                             res.statusCode = 201;
                             res.writeHead(res.statusCode, { 'Content-Type': 'application/json' });
-                            res.end(JSON.stringify(users[users.length - 1]));
+                            res.end(JSON.stringify(users.at(-1)));
                         }
                         else {
                             res.statusCode = 400;
@@ -148,6 +178,10 @@ const server = (0, http_1.createServer)((req, res) => {
                                             age: newUser.age,
                                             hobbies: newUser.hobbies
                                         };
+                                        (0, fs_1.writeFile)('src/data.json', JSON.stringify(users), (err) => {
+                                            if (err)
+                                                throw new Error('Error while writing users');
+                                        });
                                         res.statusCode = 200;
                                         res.writeHead(res.statusCode, { 'Content-Type': 'application/json' });
                                         res.end(JSON.stringify(users[i]));
@@ -196,6 +230,10 @@ const server = (0, http_1.createServer)((req, res) => {
                             const strUserId = JSON.stringify(users[i].id);
                             if (strFindId == strUserId) {
                                 users.splice(i, 1);
+                                (0, fs_1.writeFile)('src/data.json', JSON.stringify(users), (err) => {
+                                    if (err)
+                                        throw new Error('Error while writing users');
+                                });
                                 res.statusCode = 204;
                                 res.end('Record was deleted');
                             }
@@ -216,4 +254,34 @@ const server = (0, http_1.createServer)((req, res) => {
         res.end(`Sorry... Errors on the server side that occur during the processing of your request. \n Please, try again`);
     }
 });
-server.listen(port);
+if (args.length > 2 && args[2] === '--cluster=enable') {
+    const numberCPUs = (0, os_1.cpus)().length;
+    if (cluster_1.default.isPrimary) {
+        console.log(`Server running at PORT ${port}`);
+        for (let i = 0; i < numberCPUs; i += 1) {
+            cluster_1.default.fork({ WORKER_PORT: port + i + 1 });
+        }
+        cluster_1.default.on('exit', (worker, code) => {
+            (0, fs_1.writeFile)('src/data.json', "[]", (err) => {
+                if (err)
+                    throw new Error('Error while writing users');
+            });
+            console.log(`Worker ${worker.id} finished. Exit code: ${code}`);
+        });
+        cluster_1.default.on('message', (worker, message) => __awaiter(void 0, void 0, void 0, function* () {
+            console.log(message);
+        }));
+    }
+    else if (cluster_1.default.isWorker) {
+        port = Number(process.env.WORKER_PORT);
+        console.log(`Worker ${(_a = cluster_1.default.worker) === null || _a === void 0 ? void 0 : _a.id} launched at PORT ${port}`);
+        server.listen(port);
+        process.on('message', (message) => {
+        });
+    }
+}
+else {
+    server.listen(port, () => {
+        console.log(`Server running at PORT ${port}`);
+    });
+}
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
+import cluster from 'cluster';
 import { createServer } from "http";
+import { cpus } from 'os';
 import * as url from 'url';
+import { dirname, resolve } from 'path';
+import { readFile, writeFile } from 'fs';
 let uuid = require('node-uuid');
 
 interface User {
@@ -10,10 +14,15 @@ interface User {
 }
 
 const host = 'localhost';
-const port = process.env.PORT || 4000;
+let port = Number(process.env.PORT) || 4000;
 const endPoint = '/api/users';
-let users: User [] = [];
+let users: User[];
+readFile('src/data.json', (err, data) => {
+  users = JSON.parse(data.toString());
+})
 const idRegEx = /[0-9a-f]{8}(-[0-9a-f]{4}){3}-[0-9a-f]{12}$/;
+
+const args = process.argv;
 
 const server = createServer((req, res) => {
   try {
@@ -27,9 +36,12 @@ const server = createServer((req, res) => {
     if (req.method === 'GET') {
 
       if (myUrl.path === endPoint) {
-        res.statusCode = 200;
-        res.writeHead(res.statusCode, { 'Content-Type': 'application/json' });
-        res.end(JSON.stringify(users as User[]));
+        readFile('src/data.json', (err, data) => {
+          users = JSON.parse(data.toString());
+          res.statusCode = 200;
+          res.writeHead(res.statusCode, { 'Content-Type': 'application/json' });
+          res.end(JSON.stringify(users as User[]));
+        })
 
       } else if (myUrl.path?.startsWith(`${endPoint}/`)) {
 
@@ -41,26 +53,29 @@ const server = createServer((req, res) => {
 
         } else {
           const strFindId = JSON.stringify(findUserId[0]);
+          readFile('src/data.json', (err, data) => {
+            users = JSON.parse(data.toString());
+            if (users.length === 0) {
+              res.statusCode = 404;
+              res.end('Record with any userId doesn\'t exist');
+            } else {
+              users.forEach((user, i) => {
 
-          if (users.length === 0) {
-            res.statusCode = 404;
-            res.end('Record with any userId doesn\'t exist');
-          } else {
-            users.forEach((user, i) => {
-
-              const strUserId = JSON.stringify(user.id as string);
-              if (strFindId === strUserId) {
-                res.statusCode = 200;
-                res.writeHead(res.statusCode, { 'Content-Type': 'application/json' });
-                res.end(JSON.stringify(user));
-              } else {
-                if (i === users.length - 1 && strFindId !== strUserId) {
-                  res.statusCode = 404;
-                  res.end('Record with this userId doesn\'t exist');
+                const strUserId = JSON.stringify(user.id as string);
+                if (strFindId === strUserId) {
+                  res.statusCode = 200;
+                  res.writeHead(res.statusCode, { 'Content-Type': 'application/json' });
+                  res.end(JSON.stringify(user));
+                } else {
+                  if (i === users.length - 1 && strFindId !== strUserId) {
+                    res.statusCode = 404;
+                    res.end('Record with this userId doesn\'t exist');
+                  }
                 }
-              }
-            });
-          }
+              });
+            }
+          })
+
         }
 
       } else if (!myUrl.path?.startsWith(`${endPoint}/`)) {
@@ -86,9 +101,12 @@ const server = createServer((req, res) => {
             if (myUrl.path === endPoint || myUrl.path === `${endPoint}/`) {
               newUser.id = uuid.v1();
               users.push(newUser);
+              writeFile('src/data.json', JSON.stringify(users), (err) => {
+                if (err) throw new Error('Error while writing users');
+              });
               res.statusCode = 201;
               res.writeHead(res.statusCode, { 'Content-Type': 'application/json' });
-              res.end(JSON.stringify(users[users.length - 1]));
+              res.end(JSON.stringify(users.at(-1)));
             } else {
               res.statusCode = 400;
               res.end('URL is invalid');
@@ -143,7 +161,9 @@ const server = createServer((req, res) => {
                       age: newUser.age,
                       hobbies: newUser.hobbies
                     };
-
+                    writeFile('src/data.json', JSON.stringify(users), (err) => {
+                      if (err) throw new Error('Error while writing users');
+                    });
                     res.statusCode = 200;
                     res.writeHead(res.statusCode, { 'Content-Type': 'application/json' });
                     res.end(JSON.stringify(users[i]));
@@ -194,6 +214,9 @@ const server = createServer((req, res) => {
               const strUserId = JSON.stringify(users[i].id as string);
               if (strFindId == strUserId) {
                 users.splice(i, 1);
+                writeFile('src/data.json', JSON.stringify(users), (err) => {
+                  if (err) throw new Error('Error while writing users');
+                });
                 res.statusCode = 204;
                 res.end('Record was deleted');
 
@@ -217,4 +240,37 @@ const server = createServer((req, res) => {
 });
 
 
-  server.listen(port);
+if (args.length > 2 && args[2] === '--cluster=enable') {
+  const numberCPUs: number = cpus().length;
+
+  if (cluster.isPrimary) {
+    console.log(`Server running at PORT ${port}`);
+    for (let i = 0; i < numberCPUs; i += 1) {
+      cluster.fork({WORKER_PORT: port + i + 1});
+    }
+
+    cluster.on('exit', (worker, code): void => {
+      writeFile('src/data.json', "[]", (err) => {
+        if (err) throw new Error('Error while writing users');
+      });
+      console.log(
+        `Worker ${worker.id} finished. Exit code: ${code}`
+      );
+    });
+
+    cluster.on('message', async (worker, message) => {
+      console.log(message);
+    });
+  } else if (cluster.isWorker) {
+    port = Number(process.env.WORKER_PORT);
+    console.log(`Worker ${cluster.worker?.id} launched at PORT ${port}`);
+    server.listen(port);
+    process.on('message', (message) => {
+
+    })
+  }
+} else {
+  server.listen(port, () => {
+    console.log(`Server running at PORT ${port}`);
+  });
+}
